d201d1fa0b97b75daa4903b77d2ab1341d9147cc
Mohammad Rehan <mohammad.rehan.0209.0786@gmail.com>
2026-05-07 09:26:48 +0530

    Add favicon support and fix static favicon redirect

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,11 @@ from django.contrib import admin
 from django.urls import path, include  # include needed for app urls
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from smru import views as smru_views
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'smru/favicon.svg', permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('smru.urls')),  # Make sure your app name is 'smru'
 ]
